108d89c5667e867bba6fb09d80375cb05eda9709
Rayen1a <raihan01426@gmail.com>
2025-03-18 13:45:00 +0700

    Masukkan Menu

--- file2.cpp
+++ file2.cpp
@@ -8,3 +8,19 @@ return a * b;
 float luasLingkaran (float a) {
 return 3.14*a*a;
 }
+
+int main(){
+    int pilihan;
+    int panjang, lebar;
+    float jejari;
+
+    do{
+        cout << "Menu" << endl;
+        cout << "1. Luas Persegi Panjang" << endl;
+        cout << "2. Luas Lingkaran" << endl;
+        cout << "3. Luas Keluar" << endl;
+
+        cout << "Pilihan (1/2/3) = ";
+        cin >> pilihan;
+        }
+}
